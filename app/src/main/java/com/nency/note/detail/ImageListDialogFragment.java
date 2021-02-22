ad961a1cb8bb1b4815b0d187b325642c2c38baba
Nency Shobhashana <shobhashananency@gmail.com>
2021-02-22 13:36:23 -0500

    Resolved bug of permission denied

--- app/src/main/java/com/nency/note/detail/ImageListDialogFragment.java
+++ app/src/main/java/com/nency/note/detail/ImageListDialogFragment.java
@@ -166,7 +166,7 @@ public class ImageListDialogFragment extends BottomSheetDialogFragment
     }
 
     private void openGallery() {
-        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        Intent gallery = new Intent(Intent.ACTION_OPEN_DOCUMENT, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
         startActivityForResult(gallery, PICK_IMAGE);
     }
 
